e6b80e4d5239a0beec3967025c1cccc800f2cea2
karen cofre <karencofrecejas@gmail.com>
2022-02-09 19:22:52 -0300

    Initial Commit

---
+++ Enciclopedia-Stars-Wars/Enciclopedia_Stars_WarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Enciclopedia_Stars_WarsApp.swift
+//  Enciclopedia-Stars-Wars
+//
+//  Created by karen cofré on 09-02-22.
+//
+
+import SwiftUI
+
+@main
+struct Enciclopedia_Stars_WarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
